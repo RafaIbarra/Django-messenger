7f554da6f9e20a4cd61c786ac236e548378877ee
RafaIbarra <blasrafael1986@gmail.com>
2022-12-27 18:12:49 -0300

    moviendo carpeta

---
+++ static/cc.js
@@ -0,0 +1,53 @@
+
+
+// const url = 'https://django-messenger.onrender.com/mismensajes/';
+// const cards = document.getElementById("tarjeta");
+
+
+// //import {cargadato} from './chats.js'
+// const div1 = document.getElementById("conversaciones");
+// const div2 = document.getElementById("Chat");
+
+
+// fetch(url)
+// .then((resp) => resp.json())
+// .then(function(data) {
+//   let authors = data;
+ 
+//   return authors.forEach((e) => {    //< ---  recorremos data
+    
+//     let td = document.createElement("div");
+//     let us = document.createElement("div");
+//     us.setAttribute("id", e.mesajes.con);
+//     let me = document.createElement("div");
+//     let sep= document.createElement("hr");
+
+
+//     us.innerHTML = e.mesajes.con;
+//     me.innerHTML = e.mesajes.mensaje;
+//     td.appendChild(us);
+//     td.appendChild(sep);
+//     us.appendChild(me);
+//     div1.appendChild(td);
+//     // function saludo()
+//     //   {
+        
+//     //    div2.innerHTML = e.mesajes.con;
+       
+//     //    cargadato();
+        
+//     //   };
+//     // us.addEventListener('click',saludo,true);      
+//     // let men = document.createElement("span");
+//     // men.innerHTML = e.mesajes.mensaje
+//     // div2.appendChild(men);
+//      //< --- Agregamos la fila a la tabla
+  
+//   });
+
+
+
+// })
+
+
+console.log("LLEga");
