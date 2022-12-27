fbb5a4aee3f7ad1b28d64d9ad9182ad6ea4d5d3c
RafaIbarra <blasrafael1986@gmail.com>
2022-12-27 18:24:29 -0300

    agregando en urls

--- mensajeriaapp/urls.py
+++ mensajeriaapp/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from mensajes import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +29,5 @@ urlpatterns = [
     path("registroconversacion/",views.registro_conversacion,name="registro_conversacion"),
     path("mismensajes/",views.mismensajes,name="mismensajes"),
     path("chats/<str:remitente>/",views.listadochat,name="listadochat"),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+ 
