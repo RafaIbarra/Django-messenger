a36464127b8157cf9a5ab62a1fb4de5cc166e861
RafaIbarra <blasrafael1986@gmail.com>
2022-12-29 20:36:06 -0300

    prueba seleccionando foto perfil direccionando a carpeta 23

--- mensajes/models.py
+++ mensajes/models.py
@@ -88,7 +88,7 @@ class Usuarios(models.Model):
     activo=models.BooleanField(default=False)
     ultima_conexion=models.DateTimeField("fecha ultma conexion")
     fecha_registro=models.DateTimeField("fecha registro")
-    image=models.ImageField(default='sinperfil.png',blank=True,upload_to=cambiar_ruta_de_fichero)
+    image=models.ImageField(default='sinperfil.png',blank=True,upload_to=create_path)
     #image=models.CharField(max_length=200,blank=False,default='sinperfil.png')
 
     class Meta:
