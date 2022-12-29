b0ccdb78de3ed68cfdfb8d22cb7733cb072c8f75
RafaIbarra <blasrafael1986@gmail.com>
2022-12-29 18:58:40 -0300

    prueba seleccionando foto perfil direccionando a carpeta 12

--- mensajeriaapp/settings.py
+++ mensajeriaapp/settings.py
@@ -137,14 +137,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL='/static/media/'
+MEDIA_URL='/media/'
 
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     #MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media/')
-    MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static', 'media')
+    
     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+    MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static', 'media')
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
